4ca6d4f8cdf7f95fc20d1cd07ba08ee36c5e4a33
hs <tlsgustn93@naver.com>
2024-12-03 00:08:01 +0900

    fix: 에러 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,27 @@
 "use client";
-
-import React, { useEffect, useState } from "react";
-
-interface Props {
-  user: string;
-}
-
-function FunctionalComponent(props: Props) {
-  const showMessage = () => {
-    alert("Hello " + props.user);
-  };
-
-  const handleClick = () => {
-    setTimeout(showMessage, 3000); // 3초 후 실행
-  };
-
-  return <button onClick={handleClick}>Follow (Functional)</button>;
-}
-
-class ClassComponent extends React.Component<Props, {}> {
-  private showMessage = () => {
-    alert("Hello " + this.props.user);
-  };
-
-  private handleClick = () => {
-    setTimeout(this.showMessage, 3000); // 3초 후 실행
-  };
-
-  public render() {
-    return <button onClick={this.handleClick}>Follow (Class)</button>;
-  }
-}
-
+import { useEffect, useState } from "react";
+import { getCookie } from "cookies-next";
 export default function Home() {
-  const [user, setUser] = useState("haha");
-
+  // 상태 타입을 명시적으로 지정 (string | null)
+  const [token, setToken] = useState<string | null>(null);
   useEffect(() => {
-    setTimeout(() => {
-      setUser("hoho");
-    }, 2000);
+    fetch("/api/token")
+      .then((res) => res.json())
+      .then(() => {
+        // 쿠키에서 토큰 읽기
+        const tokenFromCookie = getCookie("auth-token") as string | undefined;
+        // undefined일 경우 null로 처리
+        if (tokenFromCookie) {
+          setToken(tokenFromCookie);
+        } else {
+          setToken(null);
+        }
+      });
   }, []);
-
   return (
-    <>
-      <FunctionalComponent user={user} />
-      <ClassComponent user={user} />
-    </>
+    <div>
+      <h1>Home</h1>
+      {token ? <p>Token: {token}</p> : <p>Loading token...</p>}
+    </div>
   );
 }
